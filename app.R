2f7a7189619b1a1e4b682e68dbc6a5e85f5061ef
rcruzgarcia84 <rcruzgarcia84@gmail.com>
2020-01-08 15:25:54 +0100

    Update_filter_DF.R
    Added a reactive object (the second filter now changes according to the selection of the first filter) to star making fine selection of labs possible. Best would be to have final DF from EVM to make final reactive structure.

--- app.R
+++ app.R
@@ -1,29 +1,40 @@
+
 library(shiny)
 library(leaflet)
 library(tidyverse)
 df <- read.csv(textConnection(
-  "Name,Lat,Long, content
-Dendrogreif,54.092478, 13.364295, <a href = https://botanik.uni-greifswald.de/en/landscape-ecology-and-ecosystem-dynamics/laboratories/dendroecological-lab-dendrogreif/ > Dendrogreif </a>
-Marieke&Ernst,50.985248, 13.580322, <a href = https://tu-dresden.de/bu/umwelt/forst/ww/waldwachstum/ >  Chair of Forest growth </ a>"
+  "Name,Lat,Long, Specialty1, content
+  Dendrogreif,54.092478,   13.364295, Everything,  <a href = https://botanik.uni-greifswald.de/en/landscape-ecology-and-ecosystem-dynamics/laboratories/dendroecological-lab-dendrogreif/ > Dendrogreif </a>
+  Marieke&Ernst,50.985248,  13.580322, Alles, <a href = https://tu-dresden.de/bu/umwelt/forst/ww/waldwachstum/ >  Chair of Forest growth </ a>"
 ), stringsAsFactors = FALSE)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
   div(style="display:inline-block; width: 33%",
-      selectInput("filter1", NULL, c("Filter 1" = "", LETTERS))),
+      selectInput("filter1", NULL, df$Name)),
   div(style="display:inline-block; width: 33%", 
-      selectInput("filter2", NULL, c("Filter 2" = "", LETTERS))),
+      uiOutput("filter2")),
   div(style="display:inline-block; width: 33%",
       selectInput("filter3", NULL, c(providers), 'Esri')),
-   leafletOutput("mymap", height=600)
+  leafletOutput("mymap", height=600)
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
+  
+  ### Creating a reactive object to automatically update the choices of the first two filters
+  
+  v <- reactiveValues(df = NULL)
+  
+  observeEvent(input$filter1, {
+    v$df <- df %>% filter(Name == input$filter1)
+  })
+  
+  output$filter2 <- renderUI({selectInput("filter2", NULL, v$df$Specialty1)})
+  
   output$mymap <- renderLeaflet({
     print(str(df))
-    df %>%
-      filter(Name != 'A') %>% #we do filtering here
+    v$df %>% 
       leaflet() %>%
       addProviderTiles(input$filter3,
                        options = providerTileOptions(noWrap = TRUE), group = 'Filter') %>%
@@ -39,4 +50,3 @@ server <- function(input, output, session) {
 
 # Run the application 
 shinyApp(ui = ui, server = server)
-
