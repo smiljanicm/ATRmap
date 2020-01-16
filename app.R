a59a1385871e00c7f35f6827262d97ba7e0cb119
rcruzgarcia84 <rcruzgarcia84@gmail.com>
2020-01-16 14:20:01 +0100

    Parsing_CSV_and_filter_update
    Added some lines for parsing for the CSV columns and organizing the filter function 
    (Filter 1 now is the specialty, and Filter 2 shows the available labs but does not filters anything really as of now)

--- app.R
+++ app.R
@@ -2,21 +2,19 @@
 library(shiny)
 library(leaflet)
 library(tidyverse)
-df <- read.csv(textConnection(
-  "Name,Lat,Long, Specialty1, content
-  Dendrogreif,54.092478,   13.364295, Everything,  <a href = https://botanik.uni-greifswald.de/en/landscape-ecology-and-ecosystem-dynamics/laboratories/dendroecological-lab-dendrogreif/ > Dendrogreif </a>
-  Marieke&Ernst,50.985248,  13.580322, Alles, <a href = https://tu-dresden.de/bu/umwelt/forst/ww/waldwachstum/ >  Chair of Forest growth </ a>"
-), stringsAsFactors = FALSE)
+df <- read.csv("./www/ATR tree-ring lab map.csv", stringsAsFactors = FALSE)### Place it in "www" folder in app directory
+df$content <- df %>% {paste("<a href = ", .[["Webpage.URL"]] ,">" , .[["Lab.name...Institution"]] ,  "</a>")} ### adding column with formatted string for "addCircleMarkers()"
+
+specialties <- c("Dendroecology", "Dendroclimatology", "Dendrogeomorphology", 
+                 "Dendroarchaeology", "Dendrochemistry")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
   div(style="display:inline-block; width: 33%",
-      selectInput("filter1", NULL, df$Name)),
+      selectInput("filter1", NULL, specialties)),
   div(style="display:inline-block; width: 33%", 
       uiOutput("filter2")),
-  div(style="display:inline-block; width: 33%",
-      selectInput("filter3", NULL, c(providers), 'Esri')),
-  leafletOutput("mymap", height=600)
+      leafletOutput("mymap", height=600)
 )
 
 # Define server logic required to draw a histogram
@@ -27,22 +25,29 @@ server <- function(input, output, session) {
   v <- reactiveValues(df = NULL)
   
   observeEvent(input$filter1, {
-    v$df <- df %>% filter(Name == input$filter1)
+    
+    sel_vec <- vector(mode = "numeric", length = nrow(df))
+    for(j in 1:nrow(df)){
+      sel_vec[j] <- input$filter1 %in% (df[j,]$Research.focus.of.the.lab %>% {unlist(strsplit(., split = ";"))})
+    }
+    
+    pos_vec <- which(sel_vec == TRUE)
+    v$df <- df[pos_vec,]
   })
   
-  output$filter2 <- renderUI({selectInput("filter2", NULL, v$df$Specialty1)})
+  output$filter2 <- renderUI({selectInput("filter2", NULL, ifelse(nrow(v$df) != 0, v$df$Lab.name...Institution, "None"))})
   
   output$mymap <- renderLeaflet({
     print(str(df))
     v$df %>% 
       leaflet() %>%
-      addProviderTiles(input$filter3,
+      addProviderTiles("Esri",
                        options = providerTileOptions(noWrap = TRUE), group = 'Filter') %>%
       addProviderTiles('Esri.WorldTopoMap', group = 'Topo') %>%
       addProviderTiles('Esri.WorldImagery', group = 'Sat') %>%
       addProviderTiles('Stamen.TonerLabels', group = 'Sat') %>%
       addLayersControl(baseGroups = c('Filter', 'Topo', 'Sat')) %>%
-      addCircleMarkers(~Long, ~Lat, popup=~content) %>%
+      addCircleMarkers(~Longitude, ~Latitude, popup=~content) %>%
       
       setView(10, 50, zoom = 5)
   })
