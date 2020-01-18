3be36dee1c38db248cfce7ef255b1c052088b9de
rcruzgarcia84 <rcruzgarcia84@gmail.com>
2020-01-18 15:27:00 +0100

    Final_filtering_working
    Re-structured the filtering a bit, based on creating a vector each time each filter is activated and intersecting the dataframe. Added a "Show all" option, we might consider renaming it or creating a more elegant solution.

--- app.R
+++ app.R
@@ -5,16 +5,20 @@ library(tidyverse)
 df <- read.csv("./www/ATR tree-ring lab map.csv", stringsAsFactors = FALSE)### Place it in "www" folder in app directory
 df$content <- df %>% {paste("<a href = ", .[["Webpage.URL"]] ,">" , .[["Lab.name...Institution"]] ,  "</a>")} ### adding column with formatted string for "addCircleMarkers()"
 
-specialties <- c("Dendroecology", "Dendroclimatology", "Dendrogeomorphology", 
+specialties <- c("Show all", "Dendroecology", "Dendroclimatology", "Dendrogeomorphology", 
                  "Dendroarchaeology", "Dendrochemistry")
+
+infrastructure <- c("Show all", "tree-ring widths", "wood density", "blue intensity", 
+                    "wood anatomy", "wood chemistry", "stable isotopes")
+
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
   div(style="display:inline-block; width: 33%",
-      selectInput("filter1", NULL, specialties)),
+      selectInput("filter1", NULL, specialties, selected = "Show all")),
   div(style="display:inline-block; width: 33%", 
-      uiOutput("filter2")),
-      leafletOutput("mymap", height=600)
+      selectInput("filter2", NULL, infrastructure, selected = "Show all")),
+  leafletOutput("mymap", height=600)
 )
 
 # Define server logic required to draw a histogram
@@ -24,22 +28,36 @@ server <- function(input, output, session) {
   
   v <- reactiveValues(df = NULL)
   
-  observeEvent(input$filter1, {
-    
-    sel_vec <- vector(mode = "numeric", length = nrow(df))
-    for(j in 1:nrow(df)){
-      sel_vec[j] <- input$filter1 %in% (df[j,]$Research.focus.of.the.lab %>% {unlist(strsplit(., split = ";"))})
+  pos_vec <- reactive({
+    if(input$filter1 == "Show all"){
+      
+      seq_len(nrow(df))
+      
+    }else{
+      sel_vec <- sapply(seq_len(nrow(df)), function(x){
+        input$filter1 %in% (df[x,]$Research.focus.of.the.lab %>% {unlist(strsplit(., split = ";"))})
+      })
+      which(sel_vec == TRUE)
     }
-    
-    pos_vec <- which(sel_vec == TRUE)
-    v$df <- df[pos_vec,]
   })
   
-  output$filter2 <- renderUI({selectInput("filter2", NULL, ifelse(nrow(v$df) != 0, v$df$Lab.name...Institution, "None"))})
+  pos_vec2 <- reactive({
+    if(input$filter2 == "Show all"){
+      seq_len(nrow(df))
+    }else {
+        sel_vec_filt2 <- sapply(seq_len(nrow(df)), function(x){
+        input$filter2 %in% (df[x,]$Infrastructure.is.available.for.measuring %>% {unlist(strsplit(., split = ";"))})
+      })
+      which(sel_vec_filt2 == TRUE)
+    }
+    
+  })
   
+ 
   output$mymap <- renderLeaflet({
     print(str(df))
-    v$df %>% 
+    
+     intersect(df[pos_vec(), ], df[pos_vec2(),])  %>% 
       leaflet() %>%
       addProviderTiles("Esri",
                        options = providerTileOptions(noWrap = TRUE), group = 'Filter') %>%
